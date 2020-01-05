b6fef75f032b16a184f2310005a0d567c287510a
tankmek <michael@edie.io>
2020-01-05 02:21:15 -0500

    implemented setAclToken function

--- tokendrop.ps1
+++ tokendrop.ps1
@@ -1,4 +1,3 @@
-
 function deployToken([string]$token_file, [hashtable]$endpoint) {
 <#
 .SYNOPSIS
@@ -22,31 +21,85 @@ Hashtable of computer hostnames as keys and an array of destination paths
         Write-Host "[-] Error: $token_file not found!"
         return $false
     }
+    $session = ""
     foreach ($computer in $endpoint.keys){
         Write-Host "[-] Accessing $computer" -ForegroundColor Green
+        $session = (New-PSSession -ComputerName $computer -Credential $auth)
         foreach ($dest_file in $endpoint.$computer){
             $dest_path = Split-Path -Path $dest_file
             # check if remote file already exits
             Write-Host '[-] Checking remote file'
-            $session = (New-PSSession -ComputerName $computer -Credential $auth)
-            if (! (Invoke-Command -command { Test-Path -Path $using:dest_file } -Session $session)){
+            if (! (Invoke-Command -ScriptBlock {
+                 Test-Path -Path $using:dest_file } -Session $session)){
                 Write-Host "[-] Remote file: $dest_file not present"
-                Write-Host "[-] Checking path"
+                Write-Host "[-] Checking path(s)"
                 #TODO: Add prompt if path does not exist
-                if (! (Invoke-Command -command { Test-Path -Path $using:dest_path } -Session $session)){
+                if (! (Invoke-Command -ScriptBlock {
+                     Test-Path -Path $using:dest_path } -Session $session)){
                     Write-Host "[-] Creating remote file path"
-                    Invoke-Command -Command { New-Item -ItemType Directory $using:dest_path } -Session $session
+                    Invoke-Command -Command {
+                         New-Item -ItemType Directory $using:dest_path
+                    } -Session $session
                 }
                 # Copy token to destination
                 Write-Host '[-] Copying token to destination'
                 Copy-Item -Path $token_file -Destination $dest_file -ToSession $session
+                # Set Audit ACL 
+                Write-Host '[-] Setting Audit ACL'
+                sleep 3
+                setTokenAcl $tokens.acl_tpl $session $dest_file
             } else {
                 Write-Host "[-] Token: $dest_file already exists on $computer"
             }
 
-            Remove-PSSession $session
         }
+        Remove-PSSession $session
     }
+}
+
+
+function setTokenAcl(
+<#
+.SYNOPSIS
+Sets the Audit ACL on specified honeytokens
+Author: Michael Edie @tankmek (https://blog.edie.io)
+License: BSD 3-Clause
+Required Dependencies: None
+Optional Dependencies: None
+.DESCRIPTION
+Deploys honey tokens to remote machines using hashtables for the each host
+or a specific token can be deployed to a group of hosts in a single hashtable.
+All communicaton to remote hosts uses WinRM.
+.PARAMETER dest_file
+Path a local file that has been configured with the desired audit acl
+.PARAMETER session
+PSSession of the host we have already established
+.PARAMETER dest_file
+Path to the honey token on the remote system
+#>    
+    [string]$tokenAclTemplate,
+    [System.Management.Automation.Runspaces.PSSession]$session,
+    [string]$dest_file){
+    
+    $identity = "Everyone"
+    $aclRights = "Read"
+    $aclFlags  = 3 # Success & Failure
+   
+    $AuditRights = [System.Security.AccessControl.FileSystemRights]$aclRights
+    $AuditFlags  = [System.Security.AccessControl.AuditFlags]$aclFlags
+
+    # Remote Get-Acl does not produce the desired outputs
+    #$auditAcl = Invoke-Command -ScriptBlock {Get-Acl -Path $using:file_path} -Session $session 
+    $auditAcl = Get-Acl -Path $tokenAclTemplate
+    
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAuditRule($identity,
+                  $AuditRights, $AuditFlags)
+    
+    $auditAcl.SetAuditRule($accessRule)
+    # TODO: Add try/catch
+    Invoke-Command -ScriptBlock { 
+        Set-Acl -Path $using:dest_file -AclObject $using:auditAcl
+    } -Session $session
 }
 
 Clear-Host
@@ -63,27 +116,39 @@ $banner = @'
 
 $banner
 $auth = Get-Credential
+# $assets = @{}
+$jump_box = @{}
+$file_server = @{}
+$admin_station_aws1 = @{}
+$admin_station_aws2 = @{}
+# Hash values are lists of potential destinations
 
-$assets = @{}
-# Values are list of potential destinations
-$assets.Add('winblue1', @())
-$assets.Add('winblue2', @())
-$assets.Add('winblue3', @())
+$admin_station_aws1.Add('winblue1', @())
+$admin_station_aws2.Add('winblue1', @())
+$jump_box.Add('winblue2', @())
+$file_server.Add('winblue3', @())
 # Destinations for winblue1
-$assets.winblue1 += 'c:\admin_tools\cisco-configs.zip'
-$assets.winblue1 += 'c:\admin_tools\keepass\servers.kdbx'
-$assets.winblue1 += 'c:\monkey\.aws\credentials'
-$assets.winblue1 += 'c:\monkey\.aws\configs'
+$admin_station_aws1.winblue1 += 'c:\users\marcus.jones.sa\.aws\config'
+$admin_station_aws2.winblue1 += 'c:\users\marcus.jones.sa\.aws\credentials'
 # Destination for winblue2
-$assets.winblue2 += 'c:\monkey\ham\password.txt'
+$jump_box.winblue2 += 'c:\admin_tools\keepass\servers.kdbx'
 # Destination for winblue3
-$assets.winblue3 += 'c:\monkey\vpn_configs.ovpn'
+$file_server.winblue3 += 'c:\backups\brocade_cfgs.zip'
+
 # List of tokens to choose
 $tokens = @{}
+$tokens.Add('acl_tpl', 'c:\tokens\token_tpl.txt')
 $tokens.Add('keepass', 'c:\tokens\servers.kdbx' )
-$tokens.Add('sshkey', '')
-$tokens.Add('zipfile', 'c:\tokens\cisco-config.zip')
-$tokens.Add('text_file', 'c:\tokens\yoda.txt')
+$tokens.Add('brocade_cfg', 'c:\tokens\brocade_cfgs.zip')
+$tokens.Add('aws_cfg', 'c:\tokens\.aws\config')
+$tokens.Add('aws_creds', 'c:\tokens\.aws\credentials')
 
 # Main
-deployToken $tokens.keepass $assets
+deployToken $tokens.aws_cfg $admin_station_aws1
+deployToken $tokens.aws_creds $admin_station_aws2
+deployToken $tokens.keepass $jump_box
+deployToken $tokens.brocade_cfg $file_server
+
+## TODO
+# Add ability to change file ownership to 
+# match file location if necessary
