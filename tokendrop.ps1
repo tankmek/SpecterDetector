abe13225b26aa9cea48d921d0751a7ec84a6915d
Michael Edie <michael@sawbox.net>
2020-10-08 21:59:11 -0400

    migrate to code to IDE

--- tokendrop.ps1
+++ tokendrop.ps1
@@ -1,3 +1,9 @@
+[cmdletbinding()]
+Param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("DeployTokens", "RemoveTokens")][String] $Task = "DeployTokens"
+)
+
 function deployToken([hashtable]$endpoint) {
 <#
 .SYNOPSIS
@@ -15,7 +21,7 @@ Path to the token file on the staging machine
 .PARAMETER endpoint
 Hashtable of computer hostnames as keys and an array of destination paths
 #>
-    
+
     foreach ($computer in $endpoint.keys){
         Write-Host "[-] Connecting to remote host: $computer" -ForegroundColor Green
         $session = (New-PSSession -ComputerName $computer -Credential $auth)
@@ -25,14 +31,14 @@ Hashtable of computer hostnames as keys and an array of destination paths
             $token_file = "$token_path$token"
 
             if (! (Invoke-Command -ScriptBlock {
-                 Test-Path -Path $using:dest_file } -Session $session)){
+                Test-Path -Path $using:dest_file } -Session $session)){
 
                 #TODO: Add prompt if path does not exist
                 if (! (Invoke-Command -ScriptBlock {
-                     Test-Path -Path $using:dest_path } -Session $session)){
+                    Test-Path -Path $using:dest_path } -Session $session)){
 
                     Invoke-Command -Command {
-                         New-Item -ItemType Directory $using:dest_path
+                        New-Item -ItemType Directory $using:dest_path
                     } -Session $session
                 }
                 # Check if token exists locally first
@@ -45,9 +51,9 @@ Hashtable of computer hostnames as keys and an array of destination paths
                 # Copy token to destination
                 Write-Host '[-] Dropping token:', $dest_file
                 Copy-Item -Path $token_file -Destination $dest_file -ToSession $session
-                # Set Audit ACL 
+                # Set Audit ACL
                 Write-Host '[-] Setting Audit ACL'
-                sleep 3
+                Start-Sleep 3
                 setTokenAcl $token_acl_tpl $session $dest_file
             } else {
                 Write-Host "[-] Token: $dest_file already exists on $computer"
@@ -75,35 +81,35 @@ Path a local file that has been configured with the desired audit acl
 PSSession of the host we have already established
 .PARAMETER dest_file
 Path to the honey token on the remote system
-#>    
-    [string]$tokenAclTemplate,
-    [System.Management.Automation.Runspaces.PSSession]$session,
-    [string]$dest_file){
-    
+#>
+        [string]$tokenAclTemplate,
+        [System.Management.Automation.Runspaces.PSSession]$session,
+        [string]$dest_file){
+
     $identity = "Everyone"
     $aclRights = "Read"
     $aclFlags  = 3 # Success & Failure
-   
+
     $AuditRights = [System.Security.AccessControl.FileSystemRights]$aclRights
     $AuditFlags  = [System.Security.AccessControl.AuditFlags]$aclFlags
 
     # Remote Get-Acl does not produce the desired outputs
-    #$auditAcl = Invoke-Command -ScriptBlock {Get-Acl -Path $using:file_path} -Session $session 
+    #$auditAcl = Invoke-Command -ScriptBlock {Get-Acl -Path $using:file_path} -Session $session
     $auditAcl = Get-Acl -Path $tokenAclTemplate
-    
+
     $accessRule = New-Object System.Security.AccessControl.FileSystemAuditRule($identity,
-                  $AuditRights, $AuditFlags)
-    
+    $AuditRights, $AuditFlags)
+
     $auditAcl.SetAuditRule($accessRule)
     # TODO: Add try/catch
-    Invoke-Command -ScriptBlock { 
+    Invoke-Command -ScriptBlock {
         Set-Acl -Path $using:dest_file -AclObject $using:auditAcl
     } -Session $session
 }
 
 
 function removeToken([hashtable]$remote_hosts){
-    
+
     # TODO: add -Force if user wants to do this after a warning
     foreach ($computer in $remote_hosts.keys){
         Write-Host "[-] Connecting to remote host: $computer" -ForegroundColor Green
@@ -113,12 +119,12 @@ function removeToken([hashtable]$remote_hosts){
             # check if remote file is a directory
             #Write-Host '[-] Checking remote file'
             if (! (Invoke-Command -ScriptBlock {
-                   Test-Path -Path $using:dest_file } -Session $session)){
+                Test-Path -Path $using:dest_file } -Session $session)){
                 Write-Host '[-] Remote token not found', $dest_file
                 break
-            # Does not remove directories by default        
+                # Does not remove directories by default
             } elseif ((Invoke-Command -ScriptBlock {
-                         Test-Path -Path $using:dest_file -PathType Container} -Session $session)) {
+                Test-Path -Path $using:dest_file -PathType Container} -Session $session)) {
                 Write-Host '[-] Warning: skipping directory token'
                 break
             }
@@ -135,11 +141,11 @@ function removeToken([hashtable]$remote_hosts){
 Clear-Host
 
 $banner = @'
-  __          __                   __                   
+  __          __                   __
  |  |_.-----.|  |--.-----.-----.--|  |.----.-----.-----.
  |   _|  _  ||    <|  -__|     |  _  ||   _|  _  |  _  |
  |____|_____||__|__|_____|__|__|_____||__| |_____|   __|
-                                                 |__|   
+                                                 |__|
 @tankmek
 
 
@@ -149,27 +155,28 @@ $banner
 # TODO: Add try/catch
 $auth = Get-Credential
 # Store token files here
-$token_path = 'c:\tokens\'
+$token_path = 'c:\users\mechanic\tokens\'
 $token_acl_tpl = "$token_path" + 'token_tpl.txt'
 # TODO: add option to read these from file
 # List of tokens to choose
 $key_terrain =@{}
 # Hash values are lists of potential destinations
 # TODO: capture hosts via AD OU
-$key_terrain.Add('winblue1', @())
-$key_terrain.Add('winblue2', @())
-$key_terrain.Add('winblue3', @())
+$key_terrain.Add('bruce', @())
+$key_terrain.Add('hinata', @())
+$key_terrain.Add('yagami', @())
 # Destination on host to place tokens
 # Script will create directories automatically
-$key_terrain.winblue1 += 'c:\users\marcus.jones.sa\.aws\config'
-$key_terrain.winblue1 += 'c:\users\marcus.jones.sa\.aws\credentials'
-$key_terrain.winblue2 += 'c:\admin_tools\keepass\servers.kdbx'
-$key_terrain.winblue3 += 'c:\backups\brocade_cfgs.zip'
+$key_terrain.hinata += 'c:\users\marcus.jones.sa\.aws\config'
+$key_terrain.hinata += 'c:\users\marcus.jones.sa\.aws\credentials'
+$key_terrain.bruce  += 'c:\backups\brocade_cfgs.zip'
+$key_terrain.yagami += 'c:\admin_tools\keepass\servers.kdbx'
 
 # Main
-deployToken $key_terrain
-# -- Testing --
-#removeToken $key_terrain
+switch ($Task){
+    "DeployTokens" { deployToken $key_terrain }
+    "RemoveTokens" { removeToken $key_terrain }
+}
 
 # TODO: 
 # Add feature to change file ownership to 
